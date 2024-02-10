0b5d48ce445a82ae8955a1ada9af109ef7e882e1
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-10 19:51:08 -0300

    Initial Commit

---
+++ DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by Apple Multiplier on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
